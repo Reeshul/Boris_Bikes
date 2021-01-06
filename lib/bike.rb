570a26b4c4e53f373de75389bea05c97771545d5
Reeshul <reeshul@hotmail.com>
2021-01-06 12:02:15 +0000

    Removed gemfile and initialised rspec

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,6 +1,5 @@
 class Bike
-
     def working?
+        
     end
-    
 end 
--- spec/DockingStation_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'docking_station'
-
-describe DockingStation do 
-# it "should respond to release_bike" do 
-# expect(docking_station).to respond_to(:release_bike)
-it { is_expected.to respond_to(:release_bike) }
-
-end
